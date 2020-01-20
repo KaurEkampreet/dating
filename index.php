6f36ac5f691b47ac62b1318d10a7b500414acbb5
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-01-20 03:06:45 -0500

    created home page website with css styles, images and added deafult route

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Ekampreet Kaur
+ * Date: January 19, 2020
+ * Description: This file define a default route. When a user navigates to
+ * the route of our directory they will be taken to the view that we have defined as views/home.html
+ */
+//error reporting turned on
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+
+//require vendor/autoload.php
+
+require ("vendor/autoload.php");
+
+//instantiate Fat-free
+
+$f3 = Base:: instance();
+
+//default route
+
+$f3->route('GET /', function ()
+{
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+    //echo "Pet Home";
+});
+
+//run fat free
+
+$f3-> run();
