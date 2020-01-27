770d3618e6f41a1cd005c777428237a4353fb790
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-01-27 01:01:26 -0500

    added routes and session

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
  * When a user navigates to
  * the route of our directory they will be taken to the view that we have defined as views/home.html
  */
+
+session_start();
 //error reporting turned on
 ini_set("display_errors",1);
 error_reporting(E_ALL);
@@ -20,15 +22,44 @@ require ("vendor/autoload.php");
 $f3 = Base:: instance();
 
 //default route
-
 $f3->route('GET /', function ()
 {
     $view = new Template();
     echo $view->render('views/home.html');
 
-    //echo "Pet Home";
 });
-
+//default route for personalInfo page
+$f3->route('GET /personalinformation', function () {
+    $view = new Template();
+    echo $view->render('views/personalinformation.html');
+});
+//default route for profile page
+$f3->route('POST /profile', function () {
+    $_SESSION['firstName'] = $_POST['firstName'];
+    $_SESSION['lastName'] = $_POST['lastName'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['phone'] = $_POST['phone'];
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});
+$f3->route('POST /interests', function () {
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['seeking'] = $_POST['seeking'];
+    $_SESSION['inputText'] = $_POST['inputText'];
+    $view = new Template();
+    echo $view->render('views/interests.html');
+});
+$f3->route('POST /summary', function () {
+    $interest=$_POST['interests'];
+    if(!empty($interest)) {
+        foreach ($interest as $item) {
+            $_SESSION['interests'] .= $item . " ";
+        }
+    }
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
 //run fat free
-
-$f3-> run();
+$f3->run();
