05232e397f74ec26a2cfa074ec658a399bfa23fd
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-02-23 01:58:07 -0500

    added premium member class

---
+++ classes/premiummember.php
@@ -0,0 +1,34 @@
+<?php
+class premiummember extends member
+{
+    private $_inDoorInterests;
+    private $_outDoorInterests;
+
+
+    function __construct($fName, $lName, $age, $gender, $phone, $inDoorInterests = "?", $outDoorInterests = "?")
+    {
+        parent::__construct($fName, $lName, $age, $gender, $phone);
+        $this->_inDoorInterests = $inDoorInterests;
+        $this->_outDoorInterests = $outDoorInterests;
+
+    }
+    function getIndoor()
+    {
+        return $this->_inDoorInterests;
+    }
+
+    function setIndoor($inDoorInterests)
+    {
+        $this->_inDoorInterests = $inDoorInterests;
+    }
+
+    function getOutdoor()
+    {
+        return $this->_outDoorInterests;
+    }
+
+    function setOutdoor($outDoorInterests)
+    {
+        $this->_outDoorInterests = $outDoorInterests;
+    }
+}
